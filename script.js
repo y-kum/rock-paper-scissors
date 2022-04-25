0fb40d47cf284b848185fda957b542842bc5a01d
yash <delhiiityash@gmail.com>
2022-04-25 20:37:23 +0530

    ask for user's choice

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// a function for computer's selection
 function computerPlay() {
     let num;
     let choice;
@@ -12,5 +13,33 @@ function computerPlay() {
         choice = 'Scissors';
     }
     return choice;
+}
+let conputerSelection;
+computerSelection = computerPlay();
+console.log(computerSelection);
+
+// a function for user's selection
+
+function playerPlay() {
+    input = prompt('Rock? Paper? Scissors?');
+
+    // making multiple replacements without using for loop
+
+    let caseinsensitiveRock = input.replace(/rock/gi,'Rock');
+    let caseinsensitiveRockPaper = caseinsensitiveRock.replace(/paper/gi,'Paper');
+    let caseinsensitiveRockPaperScissors = caseinsensitiveRockPaper.replace(/scissors/gi,'Scissors');
+    let playerInput;
+    if (caseinsensitiveRockPaperScissors === 'Rock' || caseinsensitiveRockPaperScissors ==='Paper' || caseinsensitiveRockPaperScissors === 'Scissors') {
+        playerInput = caseinsensitiveRockPaperScissors;
     }
-console.log(computerPlay());
+    else {
+        alert('You don\'t know how to play');
+        return ('You don\'t know how to play');
+    }
+    return playerInput;
+}
+let playerSelection = playerPlay();
+console.log(playerSelection);
+
+
+
